a97435d9cd1e0b7f68d212477b012355f12f0680
Muller <mullerdani@freemail.hu>
2015-06-13 16:15:28 +0200

    Account tábla kibővítése a create page opcióival

--- database/seeds/AccountsTableSeeder.php
+++ database/seeds/AccountsTableSeeder.php
@@ -5,13 +5,28 @@ use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 
 class AccountsTableSeeder extends Seeder {
+
     public function run()
     {
         $faker = Faker::create();
         $userIds = User::lists('id')->all();
+
         foreach(range(1, 100) as $index)
         {
+            $countq = $faker->boolean(10);
+            if($countq){
+                $count = $faker->numberBetween(2,20);
+            }
+            else {
+                $count = null;
+            }
             Account::create([
+                'countq' => $countq,
+                'count' => $count,
+                'first_owner' => $faker->boolean(70),
+                'has_email' => $faker->boolean(70),
+                'duration' => $faker->randomElement([7,14,30]),
+                'delivery' => $faker->randomFloat(2,0,72),
                 'title' => $faker->sentence(5),
                 'price'=> $faker->randomFloat(1,4,250),
                 'server'=>$faker->randomElement(['NA', 'EUNE', 'EUW', 'OCE', 'BR', 'LA', 'RU', 'TR', 'KR']),
